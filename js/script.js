67d640c70dab8879346897e5e0a26c1feb6ce8c5
Alex-N40 <alex_n40@proton.me>
2024-04-17 23:18:21 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Change color of instagram and facebook icons
+var instaIcon = document.querySelector(".fa-instagram");
+//console.log(instaIcon.outerHTML);
+instaIcon.style.color = "#d45bb9";
+
+var fbIcon = document.querySelector(".fa-facebook");
+//console.log(fbIcon.outerHTML);
+fbIcon.style.color = "#6a99b5";
+
+// Add border and padding around opening hours paragraph
+var openHours = document.querySelector("footer p");
+//console.log(openHours.outerHTML);
+openHours.style.border = "1px solid #d45bb9";
+openHours.style.padding = "1em";
+
